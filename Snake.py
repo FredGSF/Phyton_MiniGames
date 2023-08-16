ef2120f12ef98700021844cb83f814633aea6d5f
FredGSF <99353119+FredGSF@users.noreply.github.com>
2023-08-16 01:32:11 +0100

    snake 0.1

---
+++ Snake.py
@@ -0,0 +1,27 @@
+from tkinter import *
+import random
+
+GAME_WITH = 700
+GAME_HIEGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#OOFFOO"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+class snake:
+    pass
+
+class food:
+    pass
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
